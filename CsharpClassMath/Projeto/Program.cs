80a4340a0223d61999c09bba26494c21cd28ec87
lettyfranca <leeffrance@gmail.com>
2023-09-22 00:35:04 -0300

    desafio com math

---
+++ CsharpClassMath/Projeto/Program.cs
@@ -0,0 +1,9 @@
+﻿//declaração dos dados
+int primeiroValor = 155;
+int segundoValor = 357;
+
+//utilização da classe Math
+int maiorValor = Math.Max(primeiroValor,segundoValor);
+
+//saída dos dados
+Console.WriteLine($"O maior valor entre {primeiroValor} e {segundoValor} é {maiorValor}");
