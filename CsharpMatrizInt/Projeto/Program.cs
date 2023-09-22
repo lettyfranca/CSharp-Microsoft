18b5e6d609d02dd0dc46b3c4972d3ea6a814d820
lettyfranca <leeffrance@gmail.com>
2023-09-22 10:25:08 -0300

    foreach básico

---
+++ CsharpMatrizInt/Projeto/Program.cs
@@ -0,0 +1,11 @@
+﻿int[] inventario = {200, 450, 700, 175, 250};
+int sum = 0;
+int caixa = 0;
+
+foreach (int itens in inventario)
+{
+    sum += itens;
+    caixa++;
+    Console.WriteLine($"Caixa {caixa} = {itens} itens (Execução total: {sum})");
+}
+Console.WriteLine($"Nós temos {sum} itens no inventário.");
