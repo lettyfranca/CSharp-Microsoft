72f6daaccac5013739a903a8107be8cf0ed5294a
lettyfranca <leeffrance@gmail.com>
2023-09-22 09:17:54 -0300

    estruturas condicionais

---
+++ CsharpCondicional/TestProject/Program.cs
@@ -0,0 +1,38 @@
+﻿//Considerando que as regras do jogo especificam que "se a soma dos três dados, mais os bônus, é maior ou igual a 15, você ganha o jogo"
+
+//instanciar objeto da classe de gerar números aleatórios
+Random dado = new Random();
+
+//gerar os números aleatórios e salvar em variáveis
+int rolar1 = dado.Next(1,7);
+int rolar2 = dado.Next(1,7);
+int rolar3 = dado.Next(1,7);
+
+//somar os números e salver em variável
+int total = rolar1 + rolar2 + rolar3;
+
+//condicional para verificar se houve resultado duplicado = bonus
+if ((rolar1 == rolar2) || (rolar2 == rolar3) || (rolar1 == rolar3))
+{
+    Console.WriteLine("Você rolou dados iguais! +2 bônus ao total");
+    total += 2;
+}
+
+if ((rolar1 == rolar2) && (rolar2 == rolar3))
+{
+    Console.WriteLine("Você rolou 3 dados iguais! +6 bônus ao total");
+    total += 6;
+}
+
+//condicional para verificar se o usuário ganhou o jogo
+if (total >= 15)
+{
+    Console.WriteLine("Você ganhou!");
+}
+
+if (total < 15)
+{
+    Console.WriteLine("Você perdeu!");
+}
+
+Console.WriteLine($"Ao rolar os dados: {rolar1} + {rolar2} + {rolar3} = {total}");
