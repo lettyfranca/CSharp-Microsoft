0666b4ff5fae68876af6bfb7992e91f208c5db63
lettyfranca <leeffrance@gmail.com>
2023-09-21 23:59:42 -0300

    commit da primeira parte

---
+++ CsharpParte1/Projeto1/Program.cs
@@ -0,0 +1,58 @@
+﻿﻿string studentName = "Sophia Johnson";
+string course1Name = "English 101";
+string course2Name = "Algebra 101";
+string course3Name = "Biology 101";
+string course4Name = "Conputer Sciente I";
+string course5Name = "Psychology 101";
+
+int course1Credit = 3;
+int course2Credit = 3;
+int course3Credit = 4;
+int course4Credit = 4;
+int course5Credit = 3;
+
+int gradeA = 4;
+int gradeB = 3;
+
+int course1Grade = gradeA;
+int course2Grade = gradeB;
+int course3Grade = gradeB;
+int course4Grade = gradeB;
+int course5Grade = gradeA;
+
+int totalCreditHours = 0;
+
+totalCreditHours += course1Credit;
+totalCreditHours += course2Credit;
+totalCreditHours += course3Credit;
+totalCreditHours += course4Credit;
+totalCreditHours += course5Credit;
+
+//a linha abaixo tem a mesma função das atribuições acima
+//totalCreditHours = course1Credit + course2Credit + course3Credit + course4Credit + course5Credit;
+
+int totalGradePoints = 0;
+
+totalGradePoints += course1Credit * course1Grade;
+totalGradePoints += course2Credit * course2Grade;
+totalGradePoints += course3Credit * course3Grade;
+totalGradePoints += course4Credit * course4Grade;
+totalGradePoints += course5Credit * course5Grade;
+
+decimal gradePointAverage = (decimal)totalGradePoints / totalCreditHours;
+
+//Observe que, para extrair o dígito à esquerda do decimal, você o está convertendo em um valor inteiro.
+int leadingDigit = (int)gradePointAverage;
+//Na primeira metade desta operação, você move os dois dígitos decimais para a direita. Na segunda metade, você adiciona zeros ao dígito principal. Em seguida, você subtrai os dois e converte o resultado em um inteiro.
+int trailingDigits = (int)(gradePointAverage * 100) - (leadingDigit * 100);
+
+Console.WriteLine($"Student: {studentName}\n");
+Console.WriteLine("Course\t\t\t\tGrade\tCredit Hours");
+Console.WriteLine($"{course1Name}\t\t\t{course1Grade}\t\t{course1Credit}");
+Console.WriteLine($"{course2Name}\t\t\t{course2Grade}\t\t{course2Credit}");
+Console.WriteLine($"{course3Name}\t\t\t{course3Grade}\t\t{course3Credit}");
+Console.WriteLine($"{course4Name}\t\t{course4Grade}\t\t{course4Credit}");
+Console.WriteLine($"{course5Name}\t\t\t{course5Grade}\t\t{course5Credit}");
+
+Console.WriteLine($"\nFinal GPA:\t\t\t{leadingDigit}.{trailingDigits}");
+//Console.WriteLine($"{totalGradePoints} {totalCreditHours}");
--- CsharpProjects/TestProject/Program.cs
+++ CsharpProjects/TestProject/Program.cs
@@ -1,5 +1,2 @@
-﻿//o método abaixo gera um número aleatório entre 1 e 7
-Random dice = new Random();
-int roll = dice.Next(1,7);
-Console.WriteLine(roll);
-
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
