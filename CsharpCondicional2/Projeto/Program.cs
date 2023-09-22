44e2c13c5263f3fa19b6a71795a9987823b1ceb3
lettyfranca <leeffrance@gmail.com>
2023-09-22 09:49:35 -0300

    finalização condicional if else

---
+++ CsharpCondicional2/Projeto/Program.cs
@@ -0,0 +1,24 @@
+﻿//instanciar um objeto da classe random
+Random random = new Random();
+
+int daysUntilExperation = random.Next(12);
+int discountPercentage = 0;
+
+//daysUntilExperation = 15;
+
+if(daysUntilExperation == 0)
+{
+    Console.WriteLine("Your subscription has expired.");
+}
+else if(daysUntilExperation == 1)
+{
+    Console.WriteLine("Your subscription expires within a day! \nRenew now and save 20%!");
+}
+else if(daysUntilExperation <= 5)
+{
+    Console.WriteLine($"Your subscription expires in {daysUntilExperation} days. \nRenew now and save 10%!");
+}
+else if(daysUntilExperation <= 10)
+{
+    Console.WriteLine("Your subscription will expire soon. Renew now!");
+}
