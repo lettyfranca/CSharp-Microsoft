7368d436dfb23ac30710f9c3d8cac1aae89a8c69
lettyfranca <leeffrance@gmail.com>
2023-09-22 10:31:34 -0300

    desafio foreach

---
+++ CsharpMatrizes/Desafio/Program.cs
@@ -0,0 +1,9 @@
+﻿string[] pedidosFradulentos = {"B123","C234","A345","C15","B177","G3003","C235","B179"};
+
+foreach (string nome in pedidosFradulentos)
+{
+    if (nome.StartsWith("B"))
+    {
+        Console.WriteLine(nome);
+    }
+}
